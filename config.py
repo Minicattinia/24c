a8b68774c406d28e8430e189d8a3b3654bae2b7a
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2020-08-03 14:04:01 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,100 @@
+https://lemeclaim.website/auto/?r={Your EC-UserID}
+
+'''
+This config file for add autoclaim site ,You can add block for new site and
+plus number after variables.
+exsample below
+################ Site 1 #######################
+'''
+################ Site 1 #######################
+Url1 = 'https://24.sharelink.club/claim.php?coin=EXS'
+Cookie1 = 'EXSToken=EtDOwh5iEezZFDmMxsE1GSuaPExqVtDg; PHPSESSID:83dd2811d1361b61956e71103382499f'
+Delay1 = '60'
+################ Site 2 #######################
+Url2 = 'https://uptocoin.tk/ec/claim.php?coin=DASH'
+Cookie2 = 'DASHToken=Egtwdn4kL2vX4WFlsiJDYI2rnIinPDKd; PHPSESSID=1d4814b266d8bf6fcc6fd7cebe8affc8'
+Delay2 = 60
+################ Site 3 #######################
+Url3    = 'https://fastcoins.tk/claim.php?coin=DOGE'
+Cookie3 = '__cfduid=dc52c267a34688124f7dcd33bfa0512b31596184470; PHPSESSID=02b3f9ca53513cab5ab2585e6d35457a; DOGEToken=b9aFdSPHUNyXhCOzJZij8vVZj3iZqXMQ'
+Delay3  = 60  #sec only
+################ Site 4 #######################
+Url4 = 'https://uptocoin.tk/ec/claim.php?coin=DOGE'
+Cookie4 = 'DOGEToken=Pc7KrZvvPLezbxfi0Me5suJXwdOmHS35; PHPSESSID=1d4814b266d8bf6fcc6fd7cebe8affc8'
+Delay4 = 60
+################ Site 5 #######################
+Url5 = 'https://uptocoin.tk/ec/claim.php?coin=BCN'
+Cookie5 = 'BCNToken=VNXJRBVp00Gn2YpwKD5kjS1FirRWOTCR; PHPSESSID=1d4814b266d8bf6fcc6fd7cebe8affc8'
+Delay5 = 60
+################ Site 6 #######################
+Url6 = 'https://uptocoin.tk/ec/claim.php?coin=DGB'
+Cookie6 = 'DGBToken=Fbv4H5CPJ7dgXY490vRu3dE2mIOCZAcw; PHPSESSID=1d4814b266d8bf6fcc6fd7cebe8affc8'
+Delay6 = 60
+################ Site 7 #######################
+Url7 = 'https://uptocoin.tk/ec/claim.php?coin=POT'
+Cookie7 = 'POTToken=H2yGBD5rLgL3pqHUqJM903IdiS3ZKSyf; PHPSESSID=1d4814b266d8bf6fcc6fd7cebe8affc8'
+Delay7 = 60
+################ Site 8 #######################
+Url8 = 'https://uptocoin.tk/ec/claim.php?coin=TRX'
+Cookie8 = 'TRXToken=Elegin7BBp8xAh2CsvaAPNDtD3swDOKU; PHPSESSID=1d4814b266d8bf6fcc6fd7cebe8affc8'
+Delay8 = 60
+################ Site 9 #######################
+Url9 = 'https://24.sharelink.club/claim.php?coin=BCN'
+Cookie9 = 'BCNToken=42jUNUkxXLSujmqaTPjBAuG2OQ0hWyq1; PHPSESSID:83dd2811d1361b61956e71103382499f'
+Delay9 = 60
+################ Site 10 #######################
+Url10 = 'https://24.sharelink.club/claim.php?coin=EXG'
+Cookie10 = 'EXGToken=uUUd2ZzSEQIKsyoBSA9dsYtgmBOkfI3R; PHPSESSID:83dd2811d1361b61956e71103382499f'
+Delay10 = 60
+################ Site 11 #######################
+Url11 = 'https://auto.freecoin.store/claim.php?coin=EXS'
+Cookie11 = 'EXSToken=iulheFDrND7Z6NO38H1AWaUiqR9NEUEO; PHPSESSID=61aee08a857e9d8d964b84761f728009'
+Delay11 = 60
+################ Site 12 #######################
+Url12 = 'https://auto.freecoin.store/claim.php?coin=BCN'
+Cookie12 = 'BCNToken=XVLZwRWBNlpEutj98Ki1GnpGhIlJhWnS; PHPSESSID=61aee08a857e9d8d964b84761f728009'
+Delay12 = 60
+################ Site 13 #######################
+Url13 = 'https://auto.freecoin.store/claim.php?coin=RDD'
+Cookie13 = 'RDDToken=zjUrMfX6qPcQB5aaMmk078jydKvpTsqH; PHPSESSID=61aee08a857e9d8d964b84761f728009'
+Delay13 = 60
+################ Site 14 #######################
+Url14 = 'https://auto.freecoin.store/claim.php?coin=TRX'
+Cookie14 = 'TRXToken=YMMEKPtWotqMQ2giQdMdHYvvwtALRKzd; PHPSESSID=61aee08a857e9d8d964b84761f728009'
+Delay14 = 60
+################ Site 15 #######################
+Url15 = 'https://auto.freecoin.store/claim.php?coin=XTZ'
+Cookie15 = 'XTZToken=UG0uuc4saYL7Hh6j3VLTp5pq01XSBngH; PHPSESSID:61aee08a857e9d8d964b84761f728009'
+Delay15 = 60
+################ Site 16 #######################
+Url16 = 'https://lemeclaim.website/auto/claim.php?coin=LTC'
+Cookie16 = 'LTCToken=gdXu5orQbCgPULfMmg6wktRrWaIIA64o; __cfduid=df1e18d4a5cd153e109fccd3a38c0d39f1595997920; _ga=GA1.2.1980961617.1595997922; PHPSESSID=1c53775053666da4f19cfebd3e161897; _gid=GA1.2.1444237526.1596420234; adcashufpv3=14764087121735693139661950401; _data_cpc=11-1; CoinTrafficPnd0=1; _gat_gtag_UA_165296088_1=1; _data_html=23-1_30-1_77-1_117-1_586-1_587-1_760-1; _coinverti_pop_=%7B%22backed%22%3A%5B%7B%22cap%22%3A1%2C%22lastCall%22%3A1596420984087%7D%5D%7D; _pop_cca=500'
+Delay16 = 60
+################ Site 17 #######################
+Url17 = 'https://lemeclaim.website/auto/claim.php?coin=DOGE'
+Cookie17 = 'DOGEToken=Oeca8ERXAPQoNDEw5RKgkYPQyHx23jPx; __cfduid=df1e18d4a5cd153e109fccd3a38c0d39f1595997920; _ga=GA1.2.1980961617.1595997922; PHPSESSID=1c53775053666da4f19cfebd3e161897; _gid=GA1.2.1444237526.1596420234; adcashufpv3=14764087121735693139661950401; _pop_cca=500; _coinverti_pop_=%7B%22backed%22%3A%5B%7B%22cap%22%3A2%2C%22lastCall%22%3A1596421110294%7D%5D%7D; CoinTrafficPnd0=1; _data_cpc=11-2; _gat_gtag_UA_165296088_1=1; _data_html=23-1_30-1_77-2_117-1_586-1_587-4_760-2'
+Delay17 = 60
+################ Site 18 #######################
+Url18 = 'https://lemeclaim.website/auto/claim.php?coin=ADA'
+Cookie18 = 'ADAToken=cpoJ4HcwTABh41xOvL0fbheOfulK4Eh0; __cfduid=df1e18d4a5cd153e109fccd3a38c0d39f1595997920; _ga=GA1.2.1980961617.1595997922; PHPSESSID=1c53775053666da4f19cfebd3e161897; _gid=GA1.2.1444237526.1596420234; adcashufpv3=14764087121735693139661950401; _pop_cca=500; _coinverti_pop_=%7B%22backed%22%3A%5B%7B%22cap%22%3A2%2C%22lastCall%22%3A1596421110294%7D%5D%7D; _data_cpc=11-3; _data_html=23-2_30-1_77-2_117-1_586-2_587-4_760-2; _gat_gtag_UA_165296088_1=1'
+Delay18 = 60
+################ Site 19 #######################
+Url19 = 'https://lemeclaim.website/auto/claim.php?coin=RDD'
+Cookie19 = 'RDDToken=yLb56Rmd9s90JeNlwXkgDlg6KuY0wbtR; __cfduid=df1e18d4a5cd153e109fccd3a38c0d39f1595997920; _ga=GA1.2.1980961617.1595997922; PHPSESSID=1c53775053666da4f19cfebd3e161897; _gid=GA1.2.1444237526.1596420234; adcashufpv3=14764087121735693139661950401; _data_cpc=11-5; _coinverti_pop_=%7B%22backed%22%3A%5B%7B%22cap%22%3A4%2C%22lastCall%22%3A1596423912033%7D%5D%7D; _pop_cca=500; CoinTrafficPnd0=1; _data_html=23-2_30-2_77-2_117-2_586-6_587-6_760-6; _gat_gtag_UA_165296088_1=1'
+Delay19 = 60
+################ Site 20 #######################
+Url20 ='https://lemeclaim.website/auto/claim.php?coin=XTZ'
+Cookie20 = 'XTZToken=2jaoWkhTqzweRhHozgRo8s6hNqyNaICC; __cfduid=df1e18d4a5cd153e109fccd3a38c0d39f1595997920; _ga=GA1.2.1980961617.1595997922; PHPSESSID=1c53775053666da4f19cfebd3e161897; _gid=GA1.2.1444237526.1596420234; adcashufpv3=14764087121735693139661950401; _data_cpc=11-5; _coinverti_pop_=%7B%22backed%22%3A%5B%7B%22cap%22%3A4%2C%22lastCall%22%3A1596423912033%7D%5D%7D; _pop_cca=500; _data_html=23-2_30-2_77-2_117-2_586-6_587-6_760-6; _data_cpm=728-2; _gat_gtag_UA_165296088_1=1'
+Delay20 = 60
+################ Site 21 #######################
+Url21 ='https://lemeclaim.website/auto/claim.php?coin=TRX'
+Cookie21 ='TRXToken=jeTAiaVyVbipTJNpHJhM3JlkVjnSkpGT; __cfduid=df1e18d4a5cd153e109fccd3a38c0d39f1595997920; _ga=GA1.2.1980961617.1595997922; PHPSESSID=1c53775053666da4f19cfebd3e161897; _gid=GA1.2.1444237526.1596420234; adcashufpv3=14764087121735693139661950401; _data_cpc=11-5; _coinverti_pop_=%7B%22backed%22%3A%5B%7B%22cap%22%3A4%2C%22lastCall%22%3A1596423912033%7D%5D%7D; _data_html=23-2_30-2_77-2_117-2_586-6_587-6_760-6; _data_cpm=728-2; CoinTrafficPnd0=1; _gat_gtag_UA_165296088_1=1'
+Delay21 = 60
+################ Site 22 #######################
+Url22 ='https://lemeclaim.website/auto/claim.php?coin=XRP'
+Cookie22 ='XRPToken=0tRd1ZyfSNas0UWV4XLCLxOmZ9q5HOHb; __cfduid=d7867e3ab3614b456af448abf116c4f021596425878; PHPSESSID=a4857d680f10ebefbf4c7369033d95ba; _ga=GA1.2.298792070.1596425882; _gid=GA1.2.1687539435.1596425882; adcashufpv3=16928045027829833672069025383; _data_html=586-1_587-1_760-3'
+Delay22 = 60
+################ Site 23 #######################
+Url23 ='https://lemeclaim.website/auto/claim.php?coin=BCN'
+Cookie23 ='BCNToken=1Kriib2IesTKwiTifiUUXSgv2PA9kLOy; __cfduid=d7867e3ab3614b456af448abf116c4f021596425878; PHPSESSID=a4857d680f10ebefbf4c7369033d95ba; _ga=GA1.2.298792070.1596425882; _gid=GA1.2.1687539435.1596425882; adcashufpv3=16928045027829833672069025383; _data_html=586-1_587-6_760-5'
+Delay23 = 60
